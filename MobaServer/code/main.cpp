2ad7bf3387608aa097571b0e11fe5a228dbe14ad
zengjiliang <jiliangzeng@hotmail.com>
2021-01-23 13:17:56 +0800

    add files

---
+++ MobaServer/code/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "../3rd/libuv/include/uv.h"
+
+using  namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MobaGame
 MobaGameDemo
+
+Visual Studio 配置
+依赖库：ws2_32.lib Iphlpapi.lib Psapi.lib Userenv.lib
